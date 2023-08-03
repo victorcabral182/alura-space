b8be5b9821e506dc4ff47add02a4d4f48ae8944e
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2023-08-02 22:19:06 -0300

    Final commit

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Tags from "../Tags";
 import pictures from "./pictures.json";
 import styles from "./Gallery.module.scss";
 import Cards from "../Cards";
 
 export default function Gallery() {
+  const [items, setItems] = useState(pictures); //Jason das imagens
+  const tags = [...new Set(pictures.map((item) => item.tag))]; //Não os repete os itens de mesmo valor
+
+  const filterPics = (tag) => {
+    const filteredPictures = pictures.filter((pic) => {
+      return pic.tag === tag;
+    });
+    setItems(filteredPictures);
+  };
+
   return (
     <>
       <section className={styles.galeria}>
         <div>
           <h2>Browse the gallery</h2>
-          <Tags />
-          <Cards items={pictures} styles={styles} />
+          <Tags tags={tags} filterPics={filterPics} setItems={setItems} />
+          <Cards items={items} styles={styles} />
         </div>
       </section>
     </>
--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from "./Tags.module.scss";
+import pictures from "../Gallery/pictures.json";
 
-export default function Tags() {
+export default function Tags({ tags, filterPics, setItems }) {
   return (
     <div className={styles.tags}>
       <p>Search by tags:</p>
       <ul className={styles.tags__lista}>
-        <li>Stars</li>
-        <li>Galaxies</li>
-        <li>Moon</li>
-        <li>Planets</li>
+        {tags.map((tag) => {
+          return (
+            <li key={tag} onClick={() => filterPics(tag)}>
+              {tag}
+            </li>
+          );
+        })}
+        <li onClick={() => setItems(pictures)}>All</li>
       </ul>
     </div>
   );
